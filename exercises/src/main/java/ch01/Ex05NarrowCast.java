d0133fb12c8caddfd36101fe606db1d5be76b783
Denis Zakharov <dizaharov@gmail.com>
2018-09-13 15:44:22 +0300

    Narrow cast

--- exercises/src/main/java/ch01/Ex05NarrowCast.java
+++ exercises/src/main/java/ch01/Ex05NarrowCast.java
@@ -1,14 +1,24 @@
 package ch01;
 
 public class Ex05NarrowCast {
-    // TODO make narrowing conversion manually
     public static void main(String[] args) {
         int narrow = (int) Double.MAX_VALUE;
         System.out.println("Max double: " + Double.MAX_VALUE);
         System.out.println("Narrowing cast to int: " + narrow);
 
-        System.out.println(Integer.MAX_VALUE);
-        int narrow2 = (int) Math.pow(2, 32) / 2; // max int + 1
-        System.out.println(narrow2);
+        System.out.println("Max int: " + Integer.MAX_VALUE);
+        System.out.println("2^31: " + Math.pow(2, 31));
+        int narrow2 = (int) Math.pow(2, 31); // max int + 1
+        System.out.println("Narrowing case 2^31 to int: " + narrow2);
     }
 }
+
+/*
+Max double: 1.7976931348623157E308
+Narrowing cast to int: 2147483647
+Max int: 2147483647
+2^31: 2.147483648E9
+Narrowing case 2^31 to int: 2147483647
+
+Thus, cast of double > Integer.MAX_VALUE to int gives Integer.MAX_VALUE
+ */
