4eb5a28f7b553d222ef5b31ab602eaa364ab4ded
Denis Zakharov <dizaharov@gmail.com>
2018-09-23 15:21:33 +0300

    ch04-ex09 toString via reflection

---
+++ exercises/src/main/java/ch04/Ex09ToString.java
@@ -0,0 +1,24 @@
+package ch04;
+
+import ch02.point.Point;
+
+import java.lang.reflect.Field;
+
+public class Ex09ToString {
+    private static String toStringUniversal(Class<?> cls, Object obj) throws IllegalAccessException {
+        Field[] fields = cls.getDeclaredFields();
+        StringBuilder fieldsString = new StringBuilder();
+        for (int i = 0; i < fields.length; i++) {
+            Field field = fields[i];
+            field.setAccessible(true);
+            Object value = field.get(obj);
+            fieldsString.append(field.getName()).append("=").append(value);
+            if (i < fields.length - 1) fieldsString.append(", ");
+        }
+        return cls.getCanonicalName() + '{' + fieldsString.toString() + '}';
+    }
+
+    public static void main(String[] args) throws IllegalAccessException {
+        System.out.println(toStringUniversal(Point.class, new Point(3, 4)));
+    }
+}
